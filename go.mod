61d97a681fa4e92ef66ad029a63f768339139b7c
x <5vnetwork@proton.me>
2026-01-14 19:23:41 +0800

    first

---
+++ go.mod
@@ -0,0 +1,51 @@
+module github.com/5VNetwork/process-geo
+
+go 1.25.5
+
+require (
+	github.com/5vnetwork/vx-core v1.0.3
+	github.com/golang/protobuf v1.5.4
+)
+
+require (
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/apernet/quic-go v0.57.2-0.20260111184307-eec823306178 // indirect
+	github.com/bits-and-blooms/bitset v1.22.0 // indirect
+	github.com/bits-and-blooms/bloom/v3 v3.7.0 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/go-resty/resty/v2 v2.16.5 // indirect
+	github.com/google/btree v1.1.3 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/miekg/dns v1.1.66 // indirect
+	github.com/pires/go-proxyproto v0.8.1 // indirect
+	github.com/quic-go/qpack v0.6.0 // indirect
+	github.com/quic-go/quic-go v0.54.0 // indirect
+	github.com/refraction-networking/utls v1.8.1 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
+	github.com/shirou/gopsutil v2.21.11+incompatible // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/xtls/reality v0.0.0-20250527000105-e679ef7bb130 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	go.uber.org/mock v0.5.2 // indirect
+	go4.org/netipx v0.0.0-20231129151722-fdeea329fbba // indirect
+	golang.org/x/crypto v0.46.0 // indirect
+	golang.org/x/mod v0.31.0 // indirect
+	golang.org/x/net v0.48.0 // indirect
+	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/text v0.32.0 // indirect
+	golang.org/x/time v0.12.0 // indirect
+	golang.org/x/tools v0.40.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251014184007-4626949a642f // indirect
+	google.golang.org/grpc v1.75.1 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
+	gvisor.dev/gvisor v0.0.0-20250428193742-2d800c3129d5 // indirect
+)
+
+replace github.com/apernet/hysteria/core/v2 => github.com/5VNetwork/hysteria/core/v2 v2.0.0-20260113111745-7754c3d7fd99
+
+replace anytls => github.com/anytls/anytls-go v0.0.11
---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/5vnetwork/vx-core/app/util"
+	"github.com/5vnetwork/vx-core/common/geo"
+	"github.com/5vnetwork/vx-core/common/geo/memconservative"
+	"github.com/golang/protobuf/proto"
+)
+
+var geoIpUrl = "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat"
+var geoSiteUrl = "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat"
+
+func main() {
+	var err error
+	err = util.DownloadToFile(geoIpUrl, http.DefaultClient, "geoip.dat")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err =
+		util.DownloadToFile(geoSiteUrl, http.DefaultClient, "geosite.dat")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	geocode := []string{"cn", "apple-cn", "google-cn", "tld-cn", "private", "category-games", "gfw"}
+	geoipcode := []string{"private", "cn", "telegram", "google", "facebook", "twitter", "tor"}
+	geositePath := "geosite.dat"
+	geoipPath := "geoip.dat"
+	dstGeositePath := "simplified_geosite.dat"
+	dstGeoipPath := "simplified_geoip.dat"
+
+	l := memconservative.NewMemConservativeLoader()
+	geositeList := &geo.GeoSiteList{
+		Entry: []*geo.GeoSite{},
+	}
+	geoIpList := &geo.GeoIPList{
+		Entry: []*geo.GeoIP{},
+	}
+	for _, code := range geocode {
+		site, err := l.LoadSite(geositePath, code)
+		if err != nil {
+			log.Fatal(err)
+		}
+		geositeList.Entry = append(geositeList.Entry, site)
+	}
+	for _, code := range geoipcode {
+		cidr, err := l.LoadIP(geoipPath, code)
+		if err != nil {
+			log.Fatal(err)
+		}
+		geoIpList.Entry = append(geoIpList.Entry, cidr)
+	}
+
+	// write into files
+	// Marshal the geo data to protobuf format
+	geositeBytes, err := proto.Marshal(geositeList)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	geoipBytes, err := proto.Marshal(geoIpList)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tempGeosite := dstGeositePath + ".tmp"
+	err = os.WriteFile(tempGeosite, geositeBytes, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = os.Rename(tempGeosite, dstGeositePath)
+	if err != nil {
+		// Clean up temporary file if rename fails
+		os.Remove(tempGeosite)
+		log.Fatal(err)
+	}
+
+	tempGeoIpFile := dstGeoipPath + ".tmp"
+	err = os.WriteFile(tempGeoIpFile, geoipBytes, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = os.Rename(tempGeoIpFile, dstGeoipPath)
+	if err != nil {
+		// Clean up temporary file if rename fails
+		os.Remove(tempGeoIpFile)
+		log.Fatal(err)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
